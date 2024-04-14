f950538c07c54d7a8d7ad4bae4eaea44d94c23f5
Alexander Winning <alexanderw25@hotmail.com>
2024-04-13 22:24:47 -0400

    rolling back erronious changes

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 def encode(text):
-    j = ""
     for i in text:
         if int(i) < 7:
-            j += str((int(i) + 3))
+            i += 3
         else:
-            j += str((int(i) - 7))
-    return j
-
-
-def decode(encoded):
-    j = ""
-    for i in encoded:
-        if int(i) >2:
-            j += str((int(i) - 3))
-        else:
-            j += str((int(i) + 7))
-    return j
+            i -= 7
+    return text
 
 
 if __name__ == "__main__":
     option = ""
     while option != "3":
         print("""------
-1. Encode
-2. Decode
-3. Exit
+        1. Encode
+        2. Decode
+        3. Exit
         """)
         option = input("Please enter an option: ")
         if option == "1":
@@ -42,4 +31,3 @@ if __name__ == "__main__":
         else:
             print("Invalid option, try again.")
             print("")
-            
